19bf3d2e723ea03163f2fcca2c970a30fee39cb9
Syaikhul Shafwan R <heymepoo@gmail.com>
2024-02-25 15:48:08 +0700

    fix : found problem on route.show

--- start/routes.ts
+++ start/routes.ts
@@ -9,4 +9,8 @@ router.on('/login').render('pages/layouts/Auth/login')
 
 router.on('/register').render('pages/layouts/Auth/register')
 
-router.resource('siswa', SiswasController)
+router.get('siswa', [SiswasController, 'index']).as('siswa.index');
+router.post('siswa', [SiswasController, 'store']).as('siswa.store');
+router.get('siswa/:id/edit', [SiswasController, 'edit']).as('siswa.edit');
+router.put('siswa/:id', [SiswasController, 'update']).as('siswa.update');
+router.delete('siswa/:id', [SiswasController, 'destroy']).as('siswa.destroy');
